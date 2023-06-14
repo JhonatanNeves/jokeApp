ee6a423467e6fc56011cc927fb619967328d69a0
Jhonatan Neves <nevesjn2@gmail.com>
2023-06-13 22:33:34 -0300

    nav-Controller

--- app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +25,13 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home , R.id.nav_joke_day, R.id.nav_about
+            ), drawerLayout
+        )
+
+        navView.setupWithNavController(navController)
         navView.setupWithNavController(navController)
     }
 }
